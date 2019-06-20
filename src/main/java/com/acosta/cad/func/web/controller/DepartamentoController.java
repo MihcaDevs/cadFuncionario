e27de853e731726e867f6da2d8789adc94598544
Alberto Costa <mihcadevs@gmail.com>
2019-06-20 13:32:46 -0300

    Editando departamentos

--- src/main/java/com/acosta/cad/func/web/controller/DepartamentoController.java
+++ src/main/java/com/acosta/cad/func/web/controller/DepartamentoController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.acosta.cad.func.domain.Departamento;
 import com.acosta.cad.func.service.DepartamentoService;
@@ -31,6 +33,19 @@ public class DepartamentoController {
 @PostMapping("/salvar")
 public String salvar(Departamento departamento) {
 	service.salvar(departamento);
+	return "redirect:/departamentos/cadastrar";
+}
+
+@GetMapping("/editar/{id}")
+public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+	model.addAttribute("departamento", service.buscarPorId(id));
+	return "/departamento/cadastro";
+}
+
+@PostMapping("/editar")
+public String editar(Departamento departamento, RedirectAttributes attr) {
+	service.editar(departamento);
+	attr.addFlashAttribute("success", "Departamento editado com sucesso.");
 	return "redirect:/departamentos/cadastrar";
 }
 
