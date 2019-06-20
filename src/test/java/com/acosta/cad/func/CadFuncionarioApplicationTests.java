069de284f0c2e78bb61d5d98f3b5445d297ef532
Alberto Costa <mihcadevs@gmail.com>
2019-06-19 21:44:50 -0300

    Projeto criado

---
+++ src/test/java/com/acosta/cad/func/CadFuncionarioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.acosta.cad.func;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CadFuncionarioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
