100739bc06de3e57277910253c897ff970cf0213
Alberto Costa <mihcadevs@gmail.com>
2019-06-20 12:51:42 -0300

    Inserindo departamentos

--- src/main/java/com/acosta/cad/func/web/controller/DepartamentoController.java
+++ src/main/java/com/acosta/cad/func/web/controller/DepartamentoController.java
@@ -1,15 +1,23 @@
 package com.acosta.cad.func.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.acosta.cad.func.domain.Departamento;
+import com.acosta.cad.func.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 	
+	@Autowired
+	private DepartamentoService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
@@ -17,5 +25,11 @@ public class DepartamentoController {
 	public String listar() {
 		return "/departamento/lista"; 
 	}
+	
+@PostMapping("/salvar")
+public String salvar(Departamento departamento) {
+	service.salvar(departamento);
+	return "redirect:/departamentos/cadastrar";
+}
 
 }
