797ddf122df3bd744254a6452c2966c0e64a9d91
Alberto Costa <mihcadevs@gmail.com>
2019-06-23 16:06:28 -0300

    Busca funcionario por cargo

--- src/main/java/com/acosta/cad/func/service/FuncionarioServiceImpl.java
+++ src/main/java/com/acosta/cad/func/service/FuncionarioServiceImpl.java
@@ -52,4 +52,10 @@ public class FuncionarioServiceImpl implements FuncionarioService {
 		return dao.findByNome(nome);
 	}
 
+	@Override
+	public List<Funcionario> buscarPorCargo(Long id) {
+		
+		return dao.findByCargoId(id);
+	}
+
 }
