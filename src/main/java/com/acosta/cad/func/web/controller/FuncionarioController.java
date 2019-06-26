c3e42097e0ebad3d462a1b07df618feb001a1e7c
Alberto Costa <mihcadevs@gmail.com>
2019-06-25 23:09:21 -0300

    Validaçao de data com Spring Validator

--- src/main/java/com/acosta/cad/func/web/controller/FuncionarioController.java
+++ src/main/java/com/acosta/cad/func/web/controller/FuncionarioController.java
@@ -10,7 +10,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +25,7 @@ import com.acosta.cad.func.domain.Funcionario;
 import com.acosta.cad.func.domain.UF;
 import com.acosta.cad.func.service.CargoService;
 import com.acosta.cad.func.service.FuncionarioService;
+import com.acosta.cad.func.web.validator.FuncionarioValidator;
 
 
 
@@ -34,7 +37,12 @@ public class FuncionarioController {
 	private FuncionarioService funcionarioService;
 	@Autowired
 	private CargoService cargoService;
-
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new FuncionarioValidator());
+	}
+	
 	@GetMapping("/cadastrar")
 	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
@@ -52,6 +60,7 @@ public class FuncionarioController {
 		if (result.hasErrors()) {
 			return "/funcionario/cadastro";
 		}
+		
 		funcionarioService.salvar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
 		return "redirect:/funcionarios/cadastrar";
@@ -69,6 +78,7 @@ public class FuncionarioController {
 		if (result.hasErrors()) {
 			return "/funcionario/cadastro";
 		}
+		
 		funcionarioService.editar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário editado com sucesso.");
 		return "redirect:/funcionarios/cadastrar";
